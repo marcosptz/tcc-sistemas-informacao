29d15c1b0fbbe71085d22bdf7409153b612ff1f7
Marcos Petzinger de Oliveira <75101581+marcosptz@users.noreply.github.com>
2026-09-23 04:59:19 -0300

    Enhance object tracking with CUDA support
    Added support for CUDA device selection and improved tracking logic for detected objects.

--- logic.py
+++ logic.py
@@ -2,6 +2,7 @@ import math
 import time
 import cv2
 import numpy as np
+import torch
 from ultralytics import YOLO
 
 class TrackerComportamental:
@@ -20,12 +21,19 @@ class TrackerComportamental:
         if not path_lixo:
             raise ValueError("É necessário fornecer o caminho do modelo de lixo (model_path ou model_lixo_path).")
 
+        # Seleciona explicitamente GPU CUDA se disponível
+        self.device = 0 if torch.cuda.is_available() else 'cpu'
+
         # Inicializa ambos os modelos da Ultralytics
         self.model_lixo = YOLO(path_lixo)
         self.model_geral = YOLO(model_geral_path)
 
         self.limite_tempo_estatico = limite_tempo_estatico_segundos
         self.distancia_proximidade = distancia_proximidade_pixels
+        self.tempo_parado = 0
+        self.tempo_estatico_geral = time.time()
+        self.tempo_estatico = time.time()
+        self.pessoa_detectada = False
 
         # Dicionário de estado dos objetos/animais rastreados:
         # id_track -> { 'centro': (x,y), 'tempo_inicio_estatico': float, 'interacao_humana': bool, 'alerta': bool, 'tipo': str }
@@ -34,14 +42,7 @@ class TrackerComportamental:
         self.tempo_estatico_objetos = {}
 
         self.tempo_ini = time.time()
-
-        # Mapeamento de classes do modelo COCO para Pessoas e Animais
-        self.CLASSES_GERAIS_INTERESSE = {
-            16: 'Cão',
-            17: 'Gato',
-            18: 'Pessoa'
-        }
-
+        
     def _calcular_centro(self, box):
         """Calcula o ponto central (x, y) de um Bounding Box [x1, y1, x2, y2]."""
         x1, y1, x2, y2 = box
@@ -57,57 +58,74 @@ class TrackerComportamental:
         """
         tempo_atual = time.time()
         frame_desenho = frame.copy()
-        existe_pessoa = 0
 
         # -------------------------------------------------------------
         # 1. Executa Rastreamento Geral (Pessoas e Animais)
         # -------------------------------------------------------------
-        resultados_geral = self.model_geral.track(frame, persist=True, verbose=False, conf=0.25)
-
-        centros_pessoas = []
-        centros_animais = []
-        centros_objetos = []
+        resultados_geral = self.model_geral.track(frame, persist=True, verbose=False, conf=0.25, imgsz=1280, device=self.device)
 
         if resultados_geral and resultados_geral[0].boxes is not None and resultados_geral[0].boxes.id is not None:
           boxes_geral = resultados_geral[0].boxes.xyxy.cpu().numpy()
           clss_geral = resultados_geral[0].boxes.cls.cpu().numpy().astype(int)
           ids_geral = resultados_geral[0].boxes.id.cpu().numpy().astype(int)
-          # self.tempo_estatico_objetos[track_id] = tempo_atual
-          # self.tempo_estatico = tempo_atual
           self.tempo_parado = time.time() - self.tempo_ini
-          nome_classe = 'Geral'
-          cor = (255, 200, 0)  # Ciano/Amarelo
+          nomes_classes = resultados_geral[0].names
 
           for box, cls_idx, track_id in zip(boxes_geral, clss_geral, ids_geral):
-            # Desenha Bounding Box de pessoas/animais
+            nome_classe = nomes_classes.get(cls_idx, "Geral")
+            cor = (255, 200, 0)  # Azul Ciano
+            label_geral = f"{nome_classe} #{track_id}"
+            
+            # Se detectar uma pessoa, então define o tempo
+            if(nome_classe == 'person'): 
+              self.pessoa_detectada = True
+              self.tempo_estatico_geral = time.time() - self.tempo_ini
+            else:
+              self.tempo_estatico_geral = self.tempo_parado
+
+            if(nome_classe != 'person'):
+              cor = (0, 165, 255)  # Laranja (Monitorando descarte)
+              label_geral = f"{nome_classe} #{track_id} (Aguardando {int(self.tempo_estatico_geral - self.tempo_parado)}s)"
+
+            # Se Se uma pessoa for detectada e o tempo for maior que 3s, então adiciona o alerta para os animais
+            if(self.pessoa_detectada and int(self.tempo_estatico_geral - self.tempo_parado) > self.limite_tempo_estatico):
+              cor_box = (0, 0, 255)  # Vermelho Alerta
+              label = f"ALERTA: Descarte Irregular {nome_obj} #{track_id}"
+            
             x1, y1, x2, y2 = map(int, box)
             cv2.rectangle(frame_desenho, (x1, y1), (x2, y2), cor, 2)
-            cv2.putText(frame_desenho, f"{nome_classe} #{track_id}", (x1, max(y1 - 8, 15)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, cor, 2)
+            cv2.putText(frame_desenho, label_geral, (x1, max(y1 - 8, 15)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, cor, 2)
 
 
-        resultados_lixo = self.model_lixo.track(frame, persist=True, verbose=False, conf=0.20, imgsz=1280)
+        # -------------------------------------------------------------
+        # 2. Rastreamento do Modelo de Lixo
+        # -------------------------------------------------------------
+        resultados_lixo = self.model_lixo.track(frame, persist=True, verbose=False, conf=0.50, imgsz=1280, device=self.device)
 
         if resultados_lixo and resultados_lixo[0].boxes is not None and resultados_lixo[0].boxes.id is not None:
           boxes_lixo = resultados_lixo[0].boxes.xyxy.cpu().numpy()
           clss_lixo = resultados_lixo[0].boxes.cls.cpu().numpy().astype(int)
           ids_lixo = resultados_lixo[0].boxes.id.cpu().numpy().astype(int)
           nomes_classes_lixo = resultados_lixo[0].names
-          # self.tempo_estatico = tempo_atual
 
           for box, cls_idx, track_id in zip(boxes_lixo, clss_lixo, ids_lixo):
             centro_objeto = self._calcular_centro(box)
             nome_obj = nomes_classes_lixo.get(cls_idx, "Lixo")
-            centros_objetos.append({'id': track_id, 'centro': centro_objeto, 'box': box, 'tipo': nome_obj})
-            self.tempo_estatico = time.time() - self.tempo_ini
-            existe_pessoa = existe_pessoa + 1
-            # cor_box = (0, 0, 255)  # Vermelho Alerta
-            # label = f"ALERTA: Descarte Irregular #{track_id}"
+
+            # Se detectar uma pessoa, então define o tempo
+            if(self.pessoa_detectada): 
+              self.tempo_estatico = time.time() - self.tempo_ini
+            else:
+              self.tempo_estatico = self.tempo_parado
+
             cor_box = (0, 165, 255)  # Laranja (Monitorando descarte)
             label = f"{nome_obj} #{track_id} (Aguardando {int(self.tempo_estatico - self.tempo_parado)}s)"
-            # print(f"Tempo atual {tempo_parado - tempo_atual}")
-            if(int(self.tempo_estatico - self.tempo_parado) > self.limite_tempo_estatico):
+
+            # Se uma pessoa for detectada e o tempo for maior que 3s, então adiciona o alerta para as objetos
+            if(self.pessoa_detectada and int(self.tempo_estatico - self.tempo_parado) > self.limite_tempo_estatico):
               cor_box = (0, 0, 255)  # Vermelho Alerta
-              label = f"ALERTA: Descarte Irregular #{track_id}"
+              label = f"ALERTA: Descarte Irregular {nome_obj} #{track_id}"
+            
             x1, y1, x2, y2 = map(int, box)
             cv2.rectangle(frame_desenho, (x1, y1), (x2, y2), cor_box, 2)
             cv2.putText(frame_desenho, label, (x1, max(y1 - 8, 15)), cv2.FONT_HERSHEY_SIMPLEX, 0.55, cor_box, 2)
