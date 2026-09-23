e955805d16b72fa63a7fd5b829e3cf2936d09afa
Marcos Petzinger de Oliveira <75101581+marcosptz@users.noreply.github.com>
2026-09-23 18:31:26 -0300

    Simplify alert label for irregular discard
    Removed time-related information from alert label.

--- logic.py
+++ logic.py
@@ -132,7 +132,7 @@ class TrackerComportamental:
             # ---------------------------------------------------------------------------------------------------
             if(self.pessoa_detectada and int(self.tempo_estatico - self.tempo_parado) > self.limite_tempo_estatico):
               cor_box = (0, 0, 255)  # Vermelho Alerta
-              label = f"ALERTA: Descarte Irregular - {nome_obj} #{track_id} ({int(time.time() - self.tempo_pessoa)} - {int(self.tempo_estatico - self.tempo_parado)})"
+              label = f"ALERTA: Descarte Irregular - {nome_obj} #{track_id}"
             
             x1, y1, x2, y2 = map(int, box)
             cv2.rectangle(frame_desenho, (x1, y1), (x2, y2), cor_box, 2)
